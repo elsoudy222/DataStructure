98ed5e3b6569dc9b2c90c4f768900a334dafc000
soudyjr <elsoudy222@gmail.com>
2024-06-02 23:27:00 +0400

    Initial Commit

---
+++ DataStructure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStructure
+//
+//  Created by Soudy Jr on 02/06/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
