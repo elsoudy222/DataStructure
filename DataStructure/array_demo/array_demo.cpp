806850c8fecc07b245ee4128b156ea0008a3dae1
soudyjr <elsoudy222@gmail.com>
2024-06-18 00:58:18 +0400

    split array in specific file .

---
+++ DataStructure/array_demo/array_demo.cpp
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+class ArrayDemo{
+private:
+    int size;
+    int length;
+    int* items;
+    
+public:
+    ArrayDemo(int newArrSize){
+        size = newArrSize;
+        length = 0;
+        items = new int[newArrSize];
+    }
+//======================= (Fill) ==========================//
+    void Fill(){
+        int num_of_items;
+        cout<< "How Many Item Do You Want To Fill ? \n";
+        cin>> num_of_items;
+        if (num_of_items > size){
+            cout<< "You Cannot exceed the array size. \n";
+            return;
+        }else{
+            for (int i =0; i < num_of_items; i++) {
+                cout<<"Enter Item Number "<< i <<" : ";
+                cin>> items[i];
+                length++;
+            }
+        }
+    }
+//======================= (Display) ==========================//
+    void Display(){
+        cout<<"Display Array Content\n";
+        for (int i =0; i < length; i++) {
+           // cout<< i <<"|";
+            cout<< items[i] << "    ";
+        }
+        cout<<endl;
+    }
+    
+//======================= (getSize) ==========================//
+    int getSize(){
+        return  size;
+    }
+//======================= (getLength) ==========================//
+    int getLength(){
+        return length;
+    }
+//======================= (Search) ==========================//
+    int Search(int key){
+        int index = -1;
+        for (int i =0; i < length; i++) {
+            if (items[i]== key) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+//======================= (Append) ==========================//
+    void Append(int newItem){
+        if (length < size) {
+            items[length] = newItem;
+            length++;
+        }else{
+            cout<< "The Array is Full Of Elements \n";
+        }
+    }
+//======================= (Insert) ==========================//
+
+    void Insert(int index, int value){
+        if (index >=0 && index < size) {
+            for (int i = length ; i> index; i--) {
+                items[i] = items[i-1];
+            }
+            items[index] = value;
+            length ++;
+        }else{
+            cout<<"ERROR!! Index Out Of Range. \n";
+        }
+    }
+//======================= (Delete) ==========================//
+    void Delete(int index){
+        if (index >=0 && index < size) {
+            for (int i = index; i < length -1; i++) {
+                items[i] = items[i+1];
+            }
+            length--;
+        }else{
+            cout<<"ERROR!! Index Out Of Range. \n";
+        }
+    }
+//======================= (Enlarge) ==========================//
+//
+    void Enlarge(int newSize){
+        if (newSize <= size) {
+            cout<<"You Must Enter Size Larger Than old array size.";
+            return;
+        }else{
+            size = newSize;
+            int* oldArray = items;
+            items = new int[newSize];
+            for (int i =0 ; i<length; i++) {
+                items[i] = oldArray[i];
+                 
+            }
+            delete[]oldArray;
+            cout<<"The Old Array Deleted Successfuly."<<endl;
+        }
+    }
+//======================= (Merge) ==========================//
+
+    void Merge(ArrayDemo other){
+        int newSize = size + other.getSize();
+        size = newSize;
+        int* oldArray = items;
+        items = new int[newSize];
+        int i;
+        for (i = 0 ; i< length; i++) {
+            items[i] = oldArray[i];
+        }
+        delete [] oldArray;
+        
+        int j = i;
+        for (int i = 0; i < other.getLength(); i++) {
+            items[j++] = other.items[i];
+            length++;
+        }
+    }
+};
--- DataStructure/main.cpp
+++ DataStructure/main.cpp
@@ -1,117 +1,7 @@
 #include <iostream>
-#include "Array/Array.hpp"
+#include "array_demo/array_demo.cpp"
 using namespace std;
 
-class ArrayDemo{
-private:
-    int size;
-    int length;
-    int* items;
-    
-public:
-    ArrayDemo(int newArrSize){
-        size = newArrSize;
-        length = 0;
-        items = new int[newArrSize];
-    }
-//======================= (Fill) ==========================//
-    void Fill(){
-        int num_of_items;
-        cout<< "How Many Item Do You Want To Fill ? \n";
-        cin>> num_of_items;
-        if (num_of_items > size){
-            cout<< "You Cannot exceed the array size. \n";
-            return;
-        }else{
-            for (int i =0; i < num_of_items; i++) {
-                cout<<"Enter Item Number "<< i <<" : ";
-                cin>> items[i];
-                length++;
-            }
-        }
-    }
-//======================= (Display) ==========================//
-    void Display(){
-        cout<<"Display Array Content\n";
-        for (int i =0; i < length; i++) {
-            cout<< items[i] << "    ";
-        }
-        cout<<endl;
-    }
-    
-//======================= (getSize) ==========================//
-    int getSize(){
-        return  size;
-    }
-//======================= (getLength) ==========================//
-    int getLength(){
-        return length;
-    }
-//======================= (Search) ==========================//
-    int Search(int key){
-        int index = -1;
-        for (int i =0; i < length; i++) {
-            if (items[i]== key) {
-                index = i;
-                break;
-            }
-        }
-        return index;
-    }
-//======================= (Append) ==========================//
-    void Append(int newItem){
-        if (length < size) {
-            items[length] = newItem;
-            length++;
-        }else{
-            cout<< "The Array is Full Of Elements \n";
-        }
-    }
-//======================= (Insert) ==========================//
-
-//    void Insert(int index, int value){
-//        if (index >=0 && index < size) {
-//            for (int i = length ; i> index; i--) {
-//                items[i] = items[i-1];
-//            }
-//            items[index] = value;
-//            length ++;
-//        }else{
-//            cout<<"ERROR!! Index Out Of Range. \n";
-//        }
-//    }
-//======================= (Delete) ==========================//
-//    void Delete(int index){
-//        if (index >=0 && index < size) {
-//            for (int i = index; i < length -1; i++) {
-//                items[i] = items[i+1];
-//            }
-//            length--;
-//        }else{
-//            cout<<"ERROR!! Index Out Of Range. \n";
-//        }
-//    }
-//======================= (Enlarge) ==========================//
-//
-//    void Enlarge(int newSize){
-//        if (newSize <= size) {
-//            cout<<"You Must Enter Size Larger Than old array size.";
-//            return;
-//        }else{
-//            size = newSize;
-//            int* oldArray = items;
-//            items = new int[newSize];
-//            for (int i =0 ; i<length; i++) {
-//                items[i] = oldArray[i];
-//                 
-//            }
-//            delete[]oldArray;
-//        }
-//    }
-//======================= (Merge) ==========================//
-    
-    
-};
 
 
 
@@ -130,15 +20,15 @@ int main() {
     cout<<"=================================================\n";
 
 //===================== (Search) ===========================//
-    int key; // this variable for search method.
-    cout<<"Enter Value To Search : ";
-    cin>> key;
-    int index = myArray.Search(key);
-    if (index == -1) {
-        cout<< "Sorry, The Value Not Found In The Array! "<<index<<endl;
-    }else{
-        cout<< "The Value U Searced For In The Position : "<<index<<endl;
-    };
+//    int key; // this variable for search method.
+//    cout<<"Enter Value To Search : ";
+//    cin>> key;
+//    int index = myArray.Search(key);
+//    if (index == -1) {
+//        cout<< "Sorry, The Value Not Found In The Array! "<<index<<endl;
+//    }else{
+//        cout<< "The Value U Searced For In The Position : "<<index<<endl;
+//    };
     
  //   cout<<"=================================================\n";
 //======================= (Append) ==========================//
@@ -151,13 +41,14 @@ int main() {
 
 //======================= (Insert) ==========================//
 //    int value;
+//    int index;
 //    cout<<"Enter Index And Value U Want To Add : \n";
 //    cin >> index ;
 //    cin >> value ;
 //    myArray.Insert(index, value);
 //    myArray.Display();
 //    cout<<"(Array Size) = "<< myArray.getSize()<<"\t\t"<<"(Array Length) = "<<myArray.getLength()<<endl;
- //   cout<<"=================================================\n";
+    cout<<"=================================================\n";
     //======================= (Delete) ==========================//
 //    int delIndex;
 //    cout<<"Enter Index U Want To Delete : ";
@@ -171,9 +62,14 @@ int main() {
 //    myArray.Enlarge(newSize);
 //    cout<<"(Array Size) = "<< myArray.getSize()<<"\t\t"<<"(Array Length) = "<<myArray.getLength()<<endl;
 //    myArray.Display();
-//    cout<<"=================================================\n";
+    cout<<"=================================================\n";
     //======================= (Merge) ==========================//
-    
+    ArrayDemo other(3);
+    other.Fill();
+    myArray.Merge(other);
+    cout<<"(Array Size) = "<< myArray.getSize()<<"\t\t"<<"(Array Length) = "<<myArray.getLength()<<endl;
+    myArray.Display();
+    cout<<"=================================================\n";
     return 0;
 }
 
