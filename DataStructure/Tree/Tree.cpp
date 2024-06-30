cee96d6578dfd9db3aae4e9aeaf96e6c07fabefe
soudyjr <elsoudy222@gmail.com>
2024-06-30 23:57:46 +0400

    Operation Created : 1 - Insert 2- PreOrder Display 3- InOrder Display 4- postOrder Display

--- DataStructure/Tree/Tree.cpp
+++ DataStructure/Tree/Tree.cpp
@@ -8,8 +8,8 @@ public:
     TreeNode* right;
     TreeNode* left;
     
-    TreeNode(){
-        data = 0;
+    TreeNode(int value){
+        data = value;
         left = right = NULL;
     }
     
@@ -21,5 +21,52 @@ public:
     
     BST(){ root = NULL; }
     
+    TreeNode* Insert(TreeNode* r, int item){
+        if (r ==NULL) {
+            TreeNode* newNode = new TreeNode(item);
+            r = newNode;
+        }else if (item < r-> data){
+            r -> left = Insert(r->left, item);
+        }else{
+            r -> right = Insert(r->right, item);
+        }
+        return r;
+    }
     
+    void Insert(int item){
+        root = Insert(root, item);
+    }
+    
+    void PreOrder(TreeNode* r) // Root - Left - Right.
+    {
+        if (r == NULL) {
+            return;
+        }else{
+            cout<< r->data << "\t";
+            PreOrder(r->left);
+            PreOrder(r->right);
+        }
+    }
+    void InOrder(TreeNode* r) // Left - Root - Right.
+    {
+        if (r == NULL) {
+            return;
+        }else{
+            InOrder(r->left);
+            cout<< r->data << "\t";
+            InOrder(r->right);
+        }
+    }
+
+    void PostOrder(TreeNode* r) // Left - Right - Root.
+    {
+        if (r == NULL) {
+            return;
+        }else{
+            PreOrder(r->left);
+            PreOrder(r->right);
+            cout<< r->data << "\t";
+        }
+    }
+
 };
